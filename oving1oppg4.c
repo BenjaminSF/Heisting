7fb4db98ea410ed07bc965d3a135a0d49245cc9e
Benjamin <benjamin.fermann@gmail.com>
2015-01-16 14:08:46 +0100

    øvinger ++

--- oving1oppg4.c
+++ oving1oppg4.c
@@ -1,34 +1,48 @@
 
 #include <pthread.h>
 #include <stdio.h>
+int i = 0;
+pthread_mutex_t mutex;
 
-void* thread_1func(void *i){
-int k, iTemp;
-//iTemp = (int)i;
+void* thread_1func(void *tempInput){
+int k;
+
 for (k = 0; k< 1000000;k++){
-	//iTemp++;
+	pthread_mutex_lock(&mutex);	
 	i++;	
+	pthread_mutex_unlock(&mutex);
 }
+
 return NULL;
 }
 
-void* thread_2func(void *i){
-int k, iTemp;
-//iTemp = (int)i;
+void* thread_2func(void *tempInput){
+
+int k;
 for (k= 1000000; k>0;k--){
-	//iTemp--;
+	pthread_mutex_lock(&mutex);	
 	i--;
+	pthread_mutex_unlock(&mutex);
 }
+
 return NULL;
 }
 
-void main(){
-	int i = 0;	
+int main(){
+	pthread_mutex_init(&mutex, NULL);
 	pthread_t thread_1,thread_2;
-	pthread_create(&thread_1,NULL,thread_1func,(void *)&i);
-	pthread_create(&thread_2,NULL,thread_2func,(void *)&i);
+	pthread_create(&thread_1,NULL,thread_1func,NULL);
+	pthread_create(&thread_2,NULL,thread_2func,NULL);
+	int j = 0;	
+	while(j < 100)
+	{
+		usleep(1);
+		++j;
+	}
 	pthread_join(thread_1,NULL);
 	pthread_join(thread_2,NULL);
 	printf("%d\n", i);
+	pthread_mutex_destroy(&mutex);
+	return 0;
 }
 
