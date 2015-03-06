e257554d3a49a2e4d4960002f3821afbb05843d0
Even Flørenæs <even.florenes@gmail.com>
2015-03-06 13:40:26 +0100

    Laget ny driver

--- Project/elevDriver.c
+++ Project/elevDriver.c
@@ -37,19 +37,19 @@ int elevDriver_initialize(void) {
 	// Return success.
 	return 1;
 }
-void setMotorDirection(elev_motor_direction_t dirn) {
-	if (dirn == 0){
-		io_write_analog(MOTOR, 0);
-}	else if (dirn > 0) {
-		io_clear_bit(MOTORDIR);
-		io_write_analog(MOTOR, 2800);
-} 	else if (dirn < 0) {
+void setMotorDirection(motorDirection direction) {
+	if (direction < 0){
 		io_set_bit(MOTORDIR);
 		io_write_analog(MOTOR, 2800);
+}	else if (direction > 0) {
+		io_clear_bit(MOTORDIR);
+		io_write_analog(MOTOR, 2800);
+} 	else{
+		io_write_analog(MOTOR, 0);
 }
 }
 
-void setFloor();
+void setFloor(void);
 
 void setDoorOpenLamp(int status){
 	if status{
@@ -59,6 +59,67 @@ void setDoorOpenLamp(int status){
 	}
 }
 
-int isElevObstructed(){
+int isElevObstructed(void){
 	return io_read_bit(OBSTRUCTION);
 }
+
+int isElevStopped(void){
+	return io_read_bit(STOP);
+}
+
+void setStopLamp(int status){
+	if status{
+		io_set_bit(LIGHT_STOP);
+	}else{
+		io_clear_bit(LIGHT_STOP);
+	}
+}
+
+int getFloor(void){
+	int sensorFloors[4] = {SENSOR_FLOOR1,SENSOR_FLOOR2,SENSOR_FLOOR3,SENSOR_FLOOR4};
+	int i;
+	for (i = 0; i < 4; ++i)
+	{
+		if (io_read_bit(sensorFloors[i]))
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+void setFloorIndicator(int floor){
+	assert(floor >= 0);
+	assert(floor < N_FLOORS);
+	// Binary encoding. One light must always be on.
+	if (floor & 0x02)
+		io_set_bit(LIGHT_FLOOR_IND1);
+	else
+		io_clear_bit(LIGHT_FLOOR_IND1);
+	if (floor & 0x01)
+		io_set_bit(LIGHT_FLOOR_IND2);
+	else
+		io_clear_bit(LIGHT_FLOOR_IND2);
+}
+int isbuttonSignalValid(int floor, butttonType button){
+	assert(floor>= 0 && floor < N_FLOORS);
+	assert(!(button == BUTTON_CALL_UP && floor == N-1) && !(button == BUTTON_CALL_DOWN && floor == 0))
+	assert(button == BUTTON_CALL_DOWN || button == BUTTON_CALL_UP || BUTTON_COMMAND);
+	return 1;
+}
+int getButtonLamp(int floor, buttonType button){
+	if isbuttonSignalValid(floor,button){
+		return io_read_bit(button_channel_matrix[floor][button]);
+}
+	return -1;
+}
+void setButtonLamp(int floor, buttonType button, int status){
+	if isbuttonSignalValid(floor,button){
+			if (status){
+				io_set_bit(lamp_channel_matrix[floor][button]);
+			}else{
+				io_clear_bit(lamp_channel_matrix[floor][button]);
+			}
+	}
+}
+
---
+++ Project/elevDriver.h
@@ -0,0 +1,12 @@
+
+
+typedef enum tag_motorDirection {
+DIRN_DOWN = -1,
+DIRN_STOP = 0,
+DIRN_UP = 1
+} motorDirection;
+typedef enum tag_lampType {
+	BUTTON_CALL_UP = 0,
+	BUTTON_CALL_DOWN = 1,
+	BUTTON_COMMAND = 2
+} buttonType;
