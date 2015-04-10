f1d4536424c2c1410d761a88f3164d94ae6e38fe
Even Flørenæs <even.florenes@gmail.com>
2015-04-10 12:51:54 +0200

    Lagt til mulighet for fullføring av driver for en heis, men skal i costfunction for flere heiser

--- Project/main.c
+++ Project/main.c
@@ -48,7 +48,23 @@ int main() {
 					queueActive = 1;
 			}
 			}
+			/*
+			else if (getButtonSignal(i,BUTTON_CALL_UP)){
+				setButtonLamp(i,BUTTON_CALL_UP,1);
+				if(i != currentFloor){
+					nextFloor = i;
+					queueActive = 1;				//Fullfører driver for en heis
+				}									//Men skal i costfunction ved flere heiser
+			}
+			else if (getButtonSignal(i,BUTTON_CALL_DOWN)){
+				setButtonLamp(i,BUTTON_CALL_DOWN,1);
+				if (i != currentFloor){
+					nextFloor = i;
+					queueActive = 1;
+				}
+			}
 		}
+		*/
 		if(queueActive){
 			lastFloor = getFloor();
 			if(nextFloor-getFloor()> 0){
